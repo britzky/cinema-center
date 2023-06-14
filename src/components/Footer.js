1424f770f3e26412233d4960ed61a672a5ae96bd
John <johnclapper89@gmail.com>
2023-06-14 13:16:43 -0400

    added rel to footer links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,13 +7,13 @@ export const Footer = () => {
           <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <Link to="/" className="hover:underline">Cinema Center</Link></span>
         <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
             <li>
-                <a href="https://johnclapperportfolio.netlify.app" target="_blank" className="mr-4 hover:underline md:mr-6 ">My Portfolio</a>
+                <a href="https://johnclapperportfolio.netlify.app" target="_blank" rel="noreferrer" className="mr-4 hover:underline md:mr-6 ">My Portfolio</a>
             </li>
             <li>
-                <a href="https://github.com/britzky" target="_blank" className="mr-4 hover:underline md:mr-6">Github</a>
+                <a href="https://github.com/britzky" target="_blank" rel="noreferrer" className="mr-4 hover:underline md:mr-6">Github</a>
             </li>
             <li>
-                <a href="https://www.linkedin.com/in/john-clapper-476069192/" target="_blank" className="mr-4 hover:underline md:mr-6">LinkedIn</a>
+                <a href="https://www.linkedin.com/in/john-clapper-476069192/" target="_blank" rel="noreferrer" className="mr-4 hover:underline md:mr-6">LinkedIn</a>
             </li>
             <li>
                 <a href="mailto:johnclapper89@gmail.com" className="hover:underline">Contact</a>
